f757ba04e86c3c89f78b3305e4dd25e46c6118b9
broken-droid <kev_deals@ameritech.net>
2026-05-16 17:14:16 -0500

    add support for windows beta builds

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
+#ifdef MOONFIN_BETA_BUILD
+  if (!window.Create(L"Moonfin Beta", origin, size)) {
+#else
   if (!window.Create(L"Moonfin", origin, size)) {
+#endif
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
