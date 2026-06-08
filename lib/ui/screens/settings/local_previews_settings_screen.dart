95a9f16885fd7c0084e55ab31684b41057b12abe
Axl Nunez <axltico12@gmail.com>
2026-06-08 02:48:55 -0400

    added sync functionality to local previews preference toggles

--- lib/ui/screens/settings/local_previews_settings_screen.dart
+++ lib/ui/screens/settings/local_previews_settings_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:server_core/server_core.dart';
 
+import '../../../data/services/plugin_sync_service.dart';
 import '../../../l10n/app_localizations.dart';
 import '../../../preference/user_preferences.dart';
 import '../../widgets/settings/clean_settings_typography.dart';
@@ -8,6 +11,13 @@ import 'settings_app_bar.dart';
 
 class LocalPreviewsSettingsScreen extends StatelessWidget {
   const LocalPreviewsSettingsScreen({super.key});
+
+  void _pushSync() {
+    final syncService = GetIt.instance<PluginSyncService>();
+    if (syncService.pluginAvailable) {
+      syncService.pushSettings(GetIt.instance<MediaServerClient>());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +36,21 @@ class LocalPreviewsSettingsScreen extends StatelessWidget {
               title: l10n.trailerPreview,
               subtitle: l10n.autoPlayTrailers,
               icon: Icons.movie_outlined,
+              onChanged: _pushSync,
             ),
             SwitchPreferenceTile(
               preference: UserPreferences.episodePreviewEnabled,
               title: l10n.mediaPreview,
               subtitle: l10n.mediaPreviewDescription,
               icon: Icons.ondemand_video,
+              onChanged: _pushSync,
             ),
             SwitchPreferenceTile(
               preference: UserPreferences.previewAudioEnabled,
               title: l10n.previewAudio,
               subtitle: l10n.enablePreviewAudio,
               icon: Icons.volume_up,
+              onChanged: _pushSync,
             ),
           ],
         ),
